a3b37e314afa10814529265a99f6ffc7a514baa2
deng <936157065@qq.com>
2020-02-22 11:32:20 +0800

    第5次提交加上测试test

---
+++ ly-api-gateway/src/main/java/com/leyou/test/test.java
@@ -0,0 +1,15 @@
+package com.leyou.test;
+
+import javax.sound.midi.Soundbank;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("11111111111111111111");
+        deng();
+    }
+
+    public static void deng(){
+        int i = 10;
+        System.out.println(i+=i);
+    }
+}
